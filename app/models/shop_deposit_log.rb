0a65621bbe6a719d56acadc7933a9ca09d373a10
Mike <liang.wk@biaotutech.com>
2017-11-14 12:19:47 +0800

    fix typo

--- app/models/shop_deposit_log.rb
+++ app/models/shop_deposit_log.rb
@@ -1,17 +1,18 @@
 class ShopDepositLog < ActiveRecord::Base
-  
+
   enum_attr :direction, in: [
     ['plus', 1, '增加'],
     ['reduce', -1, '减少']
   ]
 
-  belongs_to :Shop
-  after_create :
+  belongs_to :shop
 
-  def shop_syn
-    self.shop.
+  # after_create :
 
-  end
+  # def shop_syn
+  #   self.shop.
+
+  # end
 
 
 end
