7777c27334c0d96833e23028ea32359e88800bc4
xiaojiawang <xiao.jiawang@biaotutech.com>
2017-11-22 16:56:33 +0800

    fix

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrdersController < Admin::BaseController
   def index
     params[:search] ||= {}
     params[:search][:created_at_gte], params[:search][:created_at_lte] = params[:search].delete(:created_at_between).split(' - ') if params[:search][:created_at_between]
-    @search =  Order.search(params[:search])
+    @search =  Order.group("orders.id").search(params[:search])
     @orders = @search.order("created_at desc").page(params[:page])
   end
 
