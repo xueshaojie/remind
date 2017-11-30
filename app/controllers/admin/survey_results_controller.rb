74369c6b4541c73125f35eb287b1ec9d87ce8693
xueshaojie <xue.shaojie@biaotutech.com>
2017-11-30 11:44:57 +0800

    添加测评历史记录

---
+++ app/controllers/admin/survey_results_controller.rb
@@ -0,0 +1,22 @@
+class Admin::SurveyResultsController < Admin::BaseController
+
+  before_filter :find_survey_result, only: [:show]
+
+  def index
+    params[:search] ||= {}
+    params[:search][:created_at_gte], params[:search][:created_at_lte] = params[:search].delete(:created_at_between).split(' - ') if params[:search][:created_at_between]
+    
+  	@search = SurveyResult.normal.search(params[:search])
+  	@survey_results = @search.page(params[:page])
+  end
+
+  def show
+  end
+
+
+  private
+    def find_survey_result
+      @survey_result = SurveyResult.normal.find(params[:id].to_i)
+    end
+
+end
--- app/models/survey_result.rb
+++ app/models/survey_result.rb
@@ -1,4 +1,15 @@
 class SurveyResult < ActiveRecord::Base
   belongs_to :wx_user
 
+  enum_attr :status, :in => [
+    ['normal',   1, '正常'],
+    ['deleted',  0, '已删除']
+  ]
+
+  enum_attr :survey_type, :in => [
+    ['energy', 1, '营销能量'],
+    ['effect', 2, '营销执行效果'],
+    ['income', 3, '企业营销收益']
+  ]
+
 end
