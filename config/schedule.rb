7c0ec7ec5f5d69cfb389153e0100bbce6b5790ee
xueshaojie <xue.shaojie@biaotutech.com>
2017-12-29 11:43:40 +0800

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-env :PATH, ENV['PATH'] #要用bundle時必須要加
+#env :PATH, ENV['PATH'] #要用bundle時必須要加
 # env :GEM_PATH, ENV['GEM_PATH']
 
 # Example:
